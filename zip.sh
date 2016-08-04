b6a5bdfc9d7c1de016dc8a57656313ee1a013baa
Manas Tungare <manas@tungare.name>
2016-08-04 08:44:45 -0700

    Clean up the zip.sh script, split into multiple functions.

--- zip.sh
+++ zip.sh
@@ -3,21 +3,44 @@
 # and creates X.hermit files out of it.
 
 
-# Clean all the .DS_Store files.
-find . -iname ".DS_Store" -exec rm "{}" \;
+function remove_ds_store() {
+  find . -iname ".DS_Store" -exec rm "{}" \;
+}
 
-# Create any missing target directories.
-ROOT_DIR=$(pwd)
-LITE_APPS_SRC_ROOT="${ROOT_DIR}/lite-apps-src"
-LITE_APPS_ZIP_ROOT="${ROOT_DIR}/lite-apps"
-mkdir -pv ${LITE_APPS_ZIP_ROOT}
+function zip_manifests() {
+  # Create any missing target directories.
+  ROOT_DIR=$(pwd)
+  LITE_APPS_SRC_ROOT="${ROOT_DIR}/lite-apps-src"
+  LITE_APPS_ZIP_ROOT="${ROOT_DIR}/lite-apps"
+  mkdir -pv "${LITE_APPS_ZIP_ROOT}"
 
-for LITE_APP in ${LITE_APPS_SRC_ROOT}/*; do
-  LITE_APP_NAME=$(basename ${LITE_APP})
-  echo ${LITE_APP_NAME}:
+  for LITE_APP in ${LITE_APPS_SRC_ROOT}/*; do
+    LITE_APP_NAME=$(basename "${LITE_APP}")
+    echo ${LITE_APP_NAME}:
+    MANIFEST_JSON="${LITE_APP}/manifest.json"
 
-  cd ${LITE_APP}  # So the path included in the zip file is just the base name of the zipped file.
-  zip -r ${LITE_APP_NAME}.hermit *
-  mv ${LITE_APP_NAME}.hermit ${LITE_APPS_ZIP_ROOT}
-  cd ${ROOT_DIR}
-done
+    check_if_exists ".manifest_version"
+    check_if_exists ".default_locale"
+    check_if_exists ".name"
+    check_if_exists ".start_url"
+    check_if_exists ".theme_color"
+    check_if_exists ".secondary_color"
+    check_if_exists ".icons[0].src"
+
+    cd "${LITE_APP}"  # So the path included in the zip file is just the base name of the zipped file.
+    zip -r "${LITE_APP_NAME}.hermit" *
+    mv "${LITE_APP_NAME}.hermit" "${LITE_APPS_ZIP_ROOT}"
+    cd "${ROOT_DIR}"
+  done
+}
+
+function check_if_exists() {
+  jq -e "$1" "${MANIFEST_JSON}" > /dev/null
+  if [ $? -ne 0 ]; then
+    echo "  Required field '$1' not found."
+    exit 1
+  fi
+}
+
+remove_ds_store
+zip_manifests
