20cf2252d11aa81ef9874bcc176dcf69ae5d6fa2
Manas Tungare <manas@tungare.name>
2016-09-16 00:36:01 -0700

    When zipping up the manifest, add files from the top-level directory directly instead of adding the entire root directory. This avoids the top-level directory name being included as a zip entry name. Skip all .DS_Store files from being zipped up into the manifest.

--- src/main/java/com/chimbori/liteapps/FileUtils.java
+++ src/main/java/com/chimbori/liteapps/FileUtils.java
@@ -3,6 +3,7 @@ package com.chimbori.liteapps;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,7 +17,16 @@ public class FileUtils {
   public static boolean zip(File rootDir, File zipFile) {
     zipFile.getParentFile().mkdirs();  // In case the target directory doesn’t exist.
     try (ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zipFile)))) {
-      addFileToZip(out, rootDir, "" /* parentDirectoryName */);
+      for (File containedFile : rootDir.listFiles(new FileFilter() {
+        @Override
+        public boolean accept(File pathname) {
+          return !pathname.getName().equals(".DS_Store");
+        }
+      })) {
+        // Add files contained under the root directory, instead of the root directory itself,
+        // so that the individual files appear at the root of the zip file instead of one directory down.
+        addFileToZip(out, containedFile, "" /* parentDirectoryName */);
+      }
       return true;
     } catch (IOException e) {
       e.printStackTrace();
