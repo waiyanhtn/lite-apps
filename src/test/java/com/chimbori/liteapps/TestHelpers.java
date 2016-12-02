953d13f2a93fe4d60465d3034432649d7884c3dc
Chimbori <hello@chimbori.com>
2016-12-02 08:41:19 -0800

    Automatically re-indent and re-format index.json when running tests.

--- src/test/java/com/chimbori/liteapps/TestHelpers.java
+++ src/test/java/com/chimbori/liteapps/TestHelpers.java
@@ -1,5 +1,13 @@
 package com.chimbori.liteapps;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonValue;
+import com.eclipsesource.json.ParseException;
+import com.eclipsesource.json.WriterConfig;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,5 +25,17 @@ class TestHelpers {
 
   public static void assertIsNotEmpty(String string) {
     assertTrue(!string.isEmpty());
+  }
+
+  static void assertJsonIsWellFormedAndReformat(File file) throws IOException {
+    try {
+      // Use a stricter parser than {@code JSONObject}, so we can catch issues such as
+      // extra commas after the last element.
+      JsonValue manifest = Json.parse(FileUtils.readFully(new FileInputStream(file)));
+      // Re-indent the <b>source file</b> by saving the JSON back to the same file.
+      FileUtils.writeFile(file, manifest.toString(WriterConfig.PRETTY_PRINT));
+    } catch (ParseException e) {
+      fail(String.format("%s: %s", file.getPath(), e.getMessage()));
+    }
   }
 }
