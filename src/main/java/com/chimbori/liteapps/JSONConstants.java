96da5a53353abd7f2f05d07a75c62601e98b776a
Chimbori <hello@chimbori.com>
2017-01-17 00:16:58 -0800

    Added code that can download a list of block-lists from various third-party sources, and combine them into a single JSON file.

--- src/main/java/com/chimbori/liteapps/JSONConstants.java
+++ src/main/java/com/chimbori/liteapps/JSONConstants.java
@@ -9,20 +9,24 @@ class JSONConstants {
     public static final String APP = "app";
 
     // Used in manifest.json.
-    public static final String NAME = "name";
-    public static final String URL = "url";
-    public static final String START_URL = "start_url";
+    public static final String BLOCK_LIST = "blocklist";
+    public static final String HOSTS = "hosts";
+    public static final String ICONS = "icons";
+    public static final String ID = "id";
     public static final String LANG = "lang";
     public static final String MANIFEST_URL = "manifest_url";
-    public static final String THEME_COLOR = "theme_color";
-    public static final String SECONDARY_COLOR = "secondary_color";
     public static final String MANIFEST_VERSION = "manifest_version";
-    public static final String ICONS = "icons";
-    public static final String RELATED_APPLICATIONS = "related_applications";
+    public static final String NAME = "name";
     public static final String PLATFORM = "platform";
-    public static final String ID = "id";
-    public static final String SRC = "src";
+    public static final String RELATED_APPLICATIONS = "related_applications";
+    public static final String SECONDARY_COLOR = "secondary_color";
     public static final String SETTINGS = "hermit_settings";
+    public static final String SOURCES = "sources";
+    public static final String SRC = "src";
+    public static final String START_URL = "start_url";
+    public static final String THEME_COLOR = "theme_color";
+    public static final String UPDATED = "updated";
+    public static final String URL = "url";
     public static final String USER_AGENT = "user_agent";
   }
 
---
+++ src/test/java/com/chimbori/liteapps/BlockListParserTest.java
@@ -0,0 +1,20 @@
+package com.chimbori.liteapps;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class BlockListParserTest {
+  @Test
+  public void testAssembleAllBlockLists() {
+    try {
+      // BlockListsParser.downloadFromSources();
+      assertTrue(BlockListsParser.packageBlockLists());
+    } catch (IOException e) {
+      fail(e.getMessage());
+    }
+  }
+}
