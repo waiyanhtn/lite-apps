ff319020b69e6b4b45967bb1c27addce83011003
Chimbori <hello@chimbori.com>
2017-04-29 18:32:05 -0700

    Even if a Lite App exists, re-generate the theme color and secondary color from its icon if the Scaffolder is intentionally invoked.

--- src/main/java/com/chimbori/liteapps/Scaffolder.java
+++ src/main/java/com/chimbori/liteapps/Scaffolder.java
@@ -12,6 +12,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -26,102 +27,105 @@ import javax.imageio.ImageIO;
  * Generates a skeleton Lite App with as many fields pre-filled as possible.
  */
 class Scaffolder {
+  private static final String MANIFEST_URL_TEMPLATE = "https://hermit.chimbori.com/lite-apps/%s.hermit";
+
   /**
    * The lite-apps.json file (containing metadata about all Lite Apps) is used as the basis for
    * generating scaffolding for the Lite App manifests.
    */
   private static boolean createScaffolding(String startUrl, String appName) throws JSONException, IOException {
-    // Create the root directory if it doesn’t exist yet.
     File liteAppDirectoryRoot = new File(FileUtils.SRC_ROOT_DIR, appName);
-    if (!liteAppDirectoryRoot.exists()) {
-      liteAppDirectoryRoot.mkdirs();
+
+    JSONObject root;
+    // If the manifest.json exists, read it before modifying, else create a new JSON object.
+    if (liteAppDirectoryRoot.exists()) {
+      File manifestJson = new File(liteAppDirectoryRoot, FileUtils.MANIFEST_JSON_FILE_NAME);
+      root = new JSONObject(FileUtils.readFully(new FileInputStream(manifestJson)));
     } else {
-      System.err.println(String.format("Lite App “%s” already exists.", appName));
-      return false;
+      // Create the root directory if it doesn’t exist yet.
+      liteAppDirectoryRoot.mkdirs();
+      root = new JSONObject();
+
+      // Constant fields, same for all apps.
+      root.put(JSONConstants.Fields.MANIFEST_VERSION, 1);
+      root.put(JSONConstants.Fields.LANG, JSONConstants.Values.EN);
+
+      // Fields that can be populated from the data provided on the command-line.
+      root.put(JSONConstants.Fields.NAME, appName);
+      root.put(JSONConstants.Fields.START_URL, startUrl);
+      root.put(JSONConstants.Fields.MANIFEST_URL, String.format(MANIFEST_URL_TEMPLATE,
+          URLEncoder.encode(appName, "UTF-8").replace("+", "%20")));
+
+      // Scrape the Web looking for RSS & Atom feeds, theme colors, and site metadata.
+      System.out.println(String.format("Fetching %s…", startUrl));
+      Scraper.SiteMetadata metadata = Scraper.scrape(startUrl);
+      System.out.println(metadata);
+
+      // Put the icon even if we don’t manage to fetch an icon successfully.
+      // This way, we can avoid additional typing, and the validator will check for the presence
+      // of the file anyway (and fail as expected).
+      root.put(JSONConstants.Fields.ICONS, new JSONArray().put(new JSONObject().put(JSONConstants.Fields.SRC, FileUtils.ICON_FILENAME)));
+
+      // TODO(manas): Fetch favicon or apple-touch-icon.
+      if (metadata.iconUrl != null && !metadata.iconUrl.isEmpty()) {
+        URL icon = new URL(metadata.iconUrl);
+        try (InputStream inputStream = icon.openStream()) {
+          Files.copy(inputStream, new File(liteAppDirectoryRoot, FileUtils.ICON_FILENAME).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+          e.printStackTrace();
+          // But still continue with the rest of the manifest generation.
+        }
+      }
+
+      // Collect bookmarkable links from likely navigation links on the page.
+      JSONArray bookmarks = new JSONArray();
+      for (Scraper.Endpoint bookmark : metadata.bookmarks) {
+        System.out.println("Adding BOOKMARK: " + bookmark.url);
+        bookmarks.put(new JSONObject()
+            .put(JSONConstants.Fields.URL, bookmark.url)
+            .put(JSONConstants.Fields.NAME, bookmark.title));
+      }
+      if (bookmarks.length() > 0) {
+        root.put("hermit_bookmarks", bookmarks);
+      }
+
+      // Collect feed URLs.
+      JSONArray feeds = new JSONArray();
+      for (Scraper.Endpoint feed : metadata.feeds) {
+        System.out.println("Adding FEED: " + feed.url);
+        feeds.put(new JSONObject()
+            .put(JSONConstants.Fields.URL, feed.url)
+            .put(JSONConstants.Fields.NAME, feed.title));
+      }
+      if (feeds.length() > 0) {
+        root.put("hermit_feeds", feeds);
+      }
+
+      // If this site has a related Android app, we can grab the ID automatically too.
+      JSONArray relatedApps = new JSONArray();
+      for (String appId : metadata.relatedApps) {
+        relatedApps.put(new JSONObject()
+            .put(JSONConstants.Fields.PLATFORM, JSONConstants.Values.PLAY)
+            .put(JSONConstants.Fields.URL, "https://play.google.com/store/apps/details?id=" + appId)
+            .put(JSONConstants.Fields.ID, appId));
+      }
+      if (relatedApps.length() > 0) {
+        root.put("related_applications", relatedApps);
+      }
     }
 
-    // If the manifest.json exists, read it before modifying, else create a new JSON object.
-    JSONObject root = new JSONObject();
-
-    // Constant fields, same for all apps.
-    root.put(JSONConstants.Fields.MANIFEST_VERSION, 1);
-    root.put(JSONConstants.Fields.LANG, JSONConstants.Values.EN);
-
-    // Fields that can be populated from the data provided on the command-line.
-    root.put(JSONConstants.Fields.NAME, appName);
-    root.put(JSONConstants.Fields.START_URL, startUrl);
-    root.put(JSONConstants.Fields.MANIFEST_URL, String.format("https://hermit.chimbori.com/lite-apps/%s.hermit",
-        URLEncoder.encode(appName, "UTF-8").replace("+", "%20")));
-
-    // Scrape the Web looking for RSS & Atom feeds, theme colors, and site metadata.
-    System.out.println(String.format("Fetching %s…", startUrl));
-    Scraper.SiteMetadata metadata = Scraper.scrape(startUrl);
-    System.out.println(metadata);
-
-    // Put the icon even if we don’t manage to fetch an icon successfully.
-    // This way, we can avoid additional typing, and the validator will check for the presence
-    // of the file anyway (and fail as expected).
-    root.put(JSONConstants.Fields.ICONS, new JSONArray().put(new JSONObject().put(JSONConstants.Fields.SRC, FileUtils.ICON_FILENAME)));
-
-    // TODO(manas): Fetch favicon or apple-touch-icon.
-    if (metadata.iconUrl != null && !metadata.iconUrl.isEmpty()) {
-      URL website = new URL(metadata.iconUrl);
-      try (InputStream inputStream = website.openStream()) {
-        Files.copy(inputStream, new File(liteAppDirectoryRoot, FileUtils.ICON_FILENAME).toPath(), StandardCopyOption.REPLACE_EXISTING);
-      } catch (IOException e) {
-        e.printStackTrace();
-        // But still continue with the rest of the manifest generation.
-      }
-
-      // Extract the color from the icon.
-      File iconFile = new File(liteAppDirectoryRoot, FileUtils.ICON_FILENAME);
-      if (iconFile.exists()) {
-        ColorExtractor.Color themeColor = ColorExtractor.getDominantColor(ImageIO.read(iconFile));
-        root.put(JSONConstants.Fields.THEME_COLOR, themeColor.toString());
-        root.put(JSONConstants.Fields.SECONDARY_COLOR, themeColor.darken(0.9f).toString());
-      }
+    // Extract the color from the icon (either newly downloaded, or from existing icon).
+    File iconFile = new File(liteAppDirectoryRoot, FileUtils.ICON_FILENAME);
+    if (iconFile.exists()) {
+      ColorExtractor.Color themeColor = ColorExtractor.getDominantColor(ImageIO.read(iconFile));
+      root.put(JSONConstants.Fields.THEME_COLOR, themeColor.toString());
+      root.put(JSONConstants.Fields.SECONDARY_COLOR, themeColor.darken(0.9f).toString());
     } else {
       // Insert a placeholder for theme_color and secondary_color so we don’t have to
       // type it in manually, but put invalid values so that the validator will catch it
       // in case we forget to replace with valid values.
       root.put(JSONConstants.Fields.THEME_COLOR, "#");
       root.put(JSONConstants.Fields.SECONDARY_COLOR, "#");
-    }
-
-    // Collect bookmarkable links from likely navigation links on the page.
-    JSONArray bookmarks = new JSONArray();
-    for (Scraper.Endpoint bookmark : metadata.bookmarks) {
-      System.out.println("Including BOOKMARK: " + bookmark.url);
-      bookmarks.put(new JSONObject()
-          .put(JSONConstants.Fields.URL, bookmark.url)
-          .put(JSONConstants.Fields.NAME, bookmark.title));
-    }
-    if (bookmarks.length() > 0) {
-      root.put("hermit_bookmarks", bookmarks);
-    }
-
-    // Collect feed URLs.
-    JSONArray feeds = new JSONArray();
-    for (Scraper.Endpoint feed : metadata.feeds) {
-      System.out.println("Including FEED: " + feed.url);
-      feeds.put(new JSONObject()
-          .put(JSONConstants.Fields.URL, feed.url)
-          .put(JSONConstants.Fields.NAME, feed.title));
-    }
-    if (feeds.length() > 0) {
-      root.put("hermit_feeds", feeds);
-    }
-
-    // If this site has a related Android app, we can grab the ID automatically too.
-    JSONArray relatedApps = new JSONArray();
-    for (String appId : metadata.relatedApps) {
-      relatedApps.put(new JSONObject()
-          .put(JSONConstants.Fields.PLATFORM, JSONConstants.Values.PLAY)
-          .put(JSONConstants.Fields.URL, "https://play.google.com/store/apps/details?id=" + appId)
-          .put(JSONConstants.Fields.ID, appId));
-    }
-    if (relatedApps.length() > 0) {
-      root.put("related_applications", relatedApps);
     }
 
     // Write the output manifest.
