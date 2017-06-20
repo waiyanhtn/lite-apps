d07f186fa672371bff69d4dcba5419ec7c3f9a41
Chimbori <hello@chimbori.com>
2017-06-19 19:23:04 -0700

    Add a constant IS_TEST_ENABLED to BlockListParserTest to avoid running it every single time.

--- src/test/java/com/chimbori/liteapps/BlockListParserTest.java
+++ src/test/java/com/chimbori/liteapps/BlockListParserTest.java
@@ -8,10 +8,16 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class BlockListParserTest {
+  private static final boolean IS_TEST_ENABLED = false;
+
   @Test
   public void testAssembleAllBlockLists() {
+    if (!IS_TEST_ENABLED) {
+      return;
+    }
+
     try {
-      // BlockListsParser.downloadFromSources();
+      BlockListsParser.downloadFromSources();
       assertTrue(BlockListsParser.packageBlockLists(false));
     } catch (IOException e) {
       fail(e.getMessage());
